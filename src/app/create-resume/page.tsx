94b65ed4b79b89fa67e9b2339129fb6193fdc9ad
aframahgoub <aframahgoub@gmail.com>
2025-04-24 21:35:07 +0000

    fix /create-resume/page

--- src/app/create-resume/page.tsx
+++ src/app/create-resume/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { useState, useRef, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
